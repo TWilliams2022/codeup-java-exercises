dac0239a40e854522e3b191c8da976024834ee01
Tyreik Williams <tyreik.mm.williams@gmail.com>
2022-04-06 14:53:11 -0400

    new

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,117 @@
 public class HelloWorld {
     public static void main (String [] args){
         System.out.println("HelloWorld!");
-    }
-}
+
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+        //        ############################################################################################
+
+
+//        String myString = "Tyreik Williams";
+//        System.out.println(myString);
+        //        ############################################################################################
+
+
+//        char myString = 'T';
+//        System.out.println(myString);
+        //        ############################################################################################
+
+//
+//        double myString = 3.14159;
+//        System.out.println(myString);
+        //        ############################################################################################
+
+
+        //variable might not be intitalized
+//        long myNumber;
+//        System.out.println(myNumber);
+        //        ############################################################################################
+
+
+        //why doesn't 3.14 work in LONG but works in INT?
+       // longs are for whole numbers
+//         long myNumber = 3.14;
+//        System.out.println(myNumber);
+        //        ############################################################################################
+
+//        int myNumber = 3.14;
+//        System.out.println(myNumber);
+       // possible lossy conversion from double to long
+        //        ############################################################################################
+
+
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+        //        ############################################################################################
+
+//
+//        long myNumber = 123;
+//        System.out.println(myNumber);
+
+//        ############################################################################################
+
+//            float myNumber = 3.14F;
+//        System.out.println(myNumber);
+        //can fix by adding a F to the end of the number
+
+//        ############################################################################################
+
+        int x = 5;
+        System.out.println(x++);  //5 because it increments once called again
+        System.out.println(x);  // 6
+
+        //        ############################################################################################
+
+
+        int c = 5;
+        System.out.println(++c); // incremented 1st add a 1 = 6
+        System.out.println(c); // new c value equals 6
+
+        //        ############################################################################################
+
+//       ststring class freetime = 2 ;
+        // cant use class it is a reserved word
+
+        //        ############################################################################################
+
+        //12
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+
+//        class java.lang.String cannot be cast to class java.lang.Integer (java.lang.String and java.lang.Integer are in module java.base of loader 'bootstrap')
+//        at HelloWorld.main(HelloWorld.java:51)
+
+//        ############################################################################################
+
+//13  write in shorthand assignment operators
+
+     //   int x = 4;
+     //   x = x + 5;
+
+        // x+=5  shorthand
+
+
+   //     int x = 3;
+   //     int y = 4;
+   //     y = y * x;
+
+        // x*=y  shorthand
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//
+//         x/=y; shorthand
+
+     //   y = y - x;
+//
+//        x-=y; shorthand
+
+        //        ############################################################################################
+
+// 15 last problem
+        //overflows into next  doesnt work
+//      int overFlow  = interger.MAX_VALUE
+//    }
+//}
